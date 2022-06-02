80ab3f3b50dd1c2ddec9b6f0e3207d40ca70e87d
Irving <irving.hernandez.g@gmail.com>
2022-06-02 18:41:46 -0400

    sql schema new

--- Database/squema.sql
+++ Database/squema.sql
@@ -1,22 +1,42 @@
--- Renewable Energy Table
-CREATE TABLE renewable_share_energy (
-  Entity varchar(100) PRIMARY KEY,
-  Code varchar(3),
-  Date_Year INTEGER,
-  Renewable_per_sub_energy DECIMAL(6,3)
+-- Table 1
+create table co2_per_capita_vs_green_energy (
+	Entity varchar(100),
+	Code varchar(4),
+	_Year int,
+	Annual_co2_per_capita decimal(7,4),
+	Renewables_percentage decimal(6,3),
+	primary key (Code)
 );
 
--- CO2 Emissions per capita
-CREATE TABLE co2_emissions_per_capita (
-  Entity varchar(100) PRIMARY KEY,
-  Code varchar(3),
-  Date_Year INTEGER,
-  Annual_CO2_emissions_per_capita DECIMAL(7,4)
+-- Table 2
+create table co2_per_capita_vs_green_electricity(
+	Entity varchar(100),
+	Code varchar(4),
+	_Year int,
+	Annual_co2_per_capita decimal(7,4),
+	Renewables_elect_per_capita decimal(6,3),
+	primary key (Code)
 );
+
+-- Table 3
+create table co2_per_capita_vs_green_energy_gen(
+	Entity varchar(100),
+	Code varchar(4),
+	_Year int,
+	Annual_co2_per_capita decimal(7,4),
+	Solar_Gen decimal(8,4),
+	Wind_Gen decimal(8,4),
+	Hydro_Gen decimal(8,4),
+	GeoBiomas_Gen decimal(8,4),
+	primary key (Code)
+)
 
 -- Query database to check successful upload
-SELECT * FROM renewable_share_energy;
-SELECT * FROM co2_emissions_per_capita;
+SELECT * FROM co2_vs_renewable_electricity;
+SELECT * FROM co2_vs_renewable_energy;
+SELECT * FROM co2_vs_renewable_energy_generation;
 
-drop table renewable_share_energy;
-drop table co2_emissions_per_capita;
+-- Delete tables
+drop table co2_vs_renewable_electricity;
+drop table co2_vs_renewable_energy;
+drop table co2_vs_renewable_energy_generation;
