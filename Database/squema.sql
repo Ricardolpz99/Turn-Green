f37257b95234689344ce532d2bca21c198958d82
Irving <irving.hernandez.g@gmail.com>
2022-06-03 21:35:51 -0400

    Joining tables

--- Database/squema.sql
+++ Database/squema.sql
@@ -1,5 +1,5 @@
 -- Table 1
-create table co2_per_capita_vs_green_energy (
+create table co2_vs_renewable_energy (
 	Entity varchar(100),
 	Code varchar(4),
 	_Year int,
@@ -9,7 +9,7 @@ create table co2_per_capita_vs_green_energy (
 );
 
 -- Table 2
-create table co2_per_capita_vs_green_electricity(
+create table co2_vs_renewable_electricity(
 	Entity varchar(100),
 	Code varchar(4),
 	_Year int,
@@ -19,7 +19,7 @@ create table co2_per_capita_vs_green_electricity(
 );
 
 -- Table 3
-create table co2_per_capita_vs_green_energy_gen(
+create table co2_vs_renewable_energy_generation(
 	Entity varchar(100),
 	Code varchar(4),
 	_Year int,
@@ -32,11 +32,53 @@ create table co2_per_capita_vs_green_energy_gen(
 )
 
 -- Query database to check successful upload
-SELECT * FROM co2_vs_renewable_electricity;
 SELECT * FROM co2_vs_renewable_energy;
+SELECT * FROM co2_vs_renewable_electricity;
 SELECT * FROM co2_vs_renewable_energy_generation;
 
 -- Delete tables
-drop table co2_vs_renewable_electricity;
 drop table co2_vs_renewable_energy;
+drop table co2_vs_renewable_electricity;
 drop table co2_vs_renewable_energy_generation;
+
+ALTER TABLE co2_vs_renewable_energy
+  DROP COLUMN index;
+
+ALTER TABLE co2_vs_renewable_energy
+  DROP COLUMN "Unnamed: 0";
+
+ALTER TABLE co2_vs_renewable_electricity
+  DROP COLUMN index;
+ 
+ALTER TABLE co2_vs_renewable_electricity
+  DROP COLUMN "Unnamed: 0";
+
+
+CREATE TABLE co2_vs_green_energy AS
+SELECT t1.* 
+  FROM co2_vs_renewable_energy t1
+    left JOIN co2_vs_renewable_electricity t2 ON 't1.Annual_co2_per_capita' = 't2.Annual_co2_per_capita'
+	
+SELECT * FROM co2_vs_green_energy;
+drop table co2_vs_green_energy;
+
+-- New join
+CREATE TABLE co2_and_green_gen_types AS
+SELECT t1.* 
+  FROM co2_vs_renewable_energy_generation t1
+    left JOIN co2_vs_renewable_electricity t2 ON 't1.Code' = 't2.Code'
+	
+SELECT * FROM co2_and_green_gen_types;
+drop table co2_and_green_gen_types;
+
+create table energies as
+SELECT t1.* 
+  FROM co2_vs_renewable_energy t1
+	LEFT JOIN co2_vs_renewable_electricity t2 ON 't1.Code' = 't2.Code'
+UNION
+SELECT t1.* 
+  FROM co2_vs_renewable_energy t1
+	RIGHT JOIN co2_vs_renewable_electricity t2 ON 't1.Code' = 't2.Code'
+	
+select * from energies;
+drop table energies;
